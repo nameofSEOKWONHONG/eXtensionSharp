0f620fd1128accf3240bb378bfb1dc9b04771019
19066Seokwon <SWHong@ecounterp.co.kr>
2022-07-15 14:49:15 +0900

    update

--- src/XCryptAES256.cs
+++ src/XCryptAES256.cs
@@ -52,6 +52,7 @@ namespace eXtensionSharp
             }
         }
         
+        [Obsolete("no more use method", true)]
         public static string xToEncryptString(this string plainText, [StringLength(32)]string key)
         {
             byte[] iv = new byte[16];
@@ -76,6 +77,7 @@ namespace eXtensionSharp
             return Convert.ToBase64String(array);
         }       
         
+        [Obsolete("no more use method", true)]
         public static string xToEncryptNumber(this string encryptString, [StringLength(36)]string key)
         {
             byte[] clearBytes = Encoding.Unicode.GetBytes(encryptString);
@@ -99,6 +101,7 @@ namespace eXtensionSharp
             return encryptString;
         } 
         
+        [Obsolete("no more use method", true)]
         public static string xToDecryptString(this string cipherText, [StringLength(32)]string key)
         {
             byte[] iv = new byte[16];
@@ -121,6 +124,7 @@ namespace eXtensionSharp
             }
         }
         
+        [Obsolete("no more use method", true)]
         public static string xToDecryptNumber(this string cipherText, [StringLength(36)]string key)
         {
             cipherText = cipherText.Replace(" ", "+");
@@ -143,6 +147,85 @@ namespace eXtensionSharp
                 }
             }
             return cipherText;
+        }
+
+        public static string xToAES256Encrypt(this string text, string key = "")
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentException("Key must have valid value.", nameof(key));
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentException("The text must have valid value.", nameof(text));
+
+            var buffer = Encoding.UTF8.GetBytes(text);
+            var hash = SHA512.Create();
+            var aesKey = new byte[24];
+            Buffer.BlockCopy(hash.ComputeHash(Encoding.UTF8.GetBytes(key)), 0, aesKey, 0, 24);
+
+            using (var aes = Aes.Create())
+            {
+                if (aes == null)
+                    throw new ArgumentException("Parameter must not be null.", nameof(aes));
+
+                aes.Key = aesKey;
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                using (var resultStream = new MemoryStream())
+                {
+                    using (var aesStream = new CryptoStream(resultStream, encryptor, CryptoStreamMode.Write))
+                    using (var plainStream = new MemoryStream(buffer))
+                    {
+                        plainStream.CopyTo(aesStream);
+                    }
+
+                    var result = resultStream.ToArray();
+                    var combined = new byte[aes.IV.Length + result.Length];
+                    Array.ConstrainedCopy(aes.IV, 0, combined, 0, aes.IV.Length);
+                    Array.ConstrainedCopy(result, 0, combined, aes.IV.Length, result.Length);
+
+                    return Convert.ToBase64String(combined);
+                }
+            }
+        }
+        public static string xToAES256Decrypt(this string encryptedText, string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentException("Key must have valid value.", nameof(key));
+            if (string.IsNullOrEmpty(encryptedText))
+                throw new ArgumentException("The encrypted text must have valid value.", nameof(encryptedText));
+
+            var combined = Convert.FromBase64String(encryptedText);
+            var buffer = new byte[combined.Length];
+            var hash = SHA512.Create();
+            var aesKey = new byte[24];
+            Buffer.BlockCopy(hash.ComputeHash(Encoding.UTF8.GetBytes(key)), 0, aesKey, 0, 24);
+
+            using (var aes = Aes.Create())
+            {
+                if (aes == null)
+                    throw new ArgumentException("Parameter must not be null.", nameof(aes));
+
+                aes.Key = aesKey;
+
+                var iv = new byte[aes.IV.Length];
+                var ciphertext = new byte[buffer.Length - iv.Length];
+
+                Array.ConstrainedCopy(combined, 0, iv, 0, iv.Length);
+                Array.ConstrainedCopy(combined, iv.Length, ciphertext, 0, ciphertext.Length);
+
+                aes.IV = iv;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                using (var resultStream = new MemoryStream())
+                {
+                    using (var aesStream = new CryptoStream(resultStream, decryptor, CryptoStreamMode.Write))
+                    using (var plainStream = new MemoryStream(ciphertext))
+                    {
+                        plainStream.CopyTo(aesStream);
+                    }
+
+                    return Encoding.UTF8.GetString(resultStream.ToArray());
+                }
+            }
         }
     }
 }
