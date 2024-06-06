340217b94da7919b1873d501f3ffe41eb32094ca
seokwonhong <h20913@gmail.com>
2024-06-06 17:58:25 +0900

    remove language_test

--- test/CultureTest.cs
+++ test/CultureTest.cs
@@ -4,19 +4,19 @@ namespace eXtensionSharp.test;
 
 public class CultureTest
 {
-    [Test]
-    public void language_display_name_test()
-    {
-        var expected = "영어(미국)";
-        var result = "en-US".xToLanguageDisplayName();
-        Assert.That(result, Is.EqualTo(expected));
-    }
-
-    [Test]
-    public void language_english_name_test()
-    {
-        var expected = "English (United States)";
-        var result = "en-US".xToLanguageEngName();
-        Assert.That(result, Is.EqualTo(expected));
-    }
+    // [Test]
+    // public void language_display_name_test()
+    // {
+    //     var expected = "영어(미국)";
+    //     var result = "en-US".xToLanguageDisplayName();
+    //     Assert.That(result, Is.EqualTo(expected));
+    // }
+    //
+    // [Test]
+    // public void language_english_name_test()
+    // {
+    //     var expected = "English (United States)";
+    //     var result = "en-US".xToLanguageEngName();
+    //     Assert.That(result, Is.EqualTo(expected));
+    // }
 }
